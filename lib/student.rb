fe556639f330fd10129fa2874540f86ba9d45ed2
ocawthorne <ocawthorne@gmail.com>
2020-10-29 19:48:00 +0000

    orms-classes-291020

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,46 @@
+require 'pry'
 class Student
+
+  attr_reader :id
+  attr_accessor :name, :grade
+
+  def initialize(name, grade)
+    @id = id
+    @name = name
+    @grade = grade
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+      )
+      SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade) VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+
+  def self.create(student)
+    new_student = Student.new(student[:name], student[:grade])
+    new_student.save
+    new_student
+  end
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
